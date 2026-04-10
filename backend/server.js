ef5d01e38c15978129d25bfcdcc8134098eb4393
Your Name <bhabishamajhi1@gmail.com>
2026-04-10 22:01:55 +0545

    fix render routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -19,4 +20,4 @@ app.get("/", (req, res) => {
   res.send("Backend is running successfully 🚀");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
