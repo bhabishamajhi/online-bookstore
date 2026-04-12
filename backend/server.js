4df143bfc0ca4cd4aa5c13d381be06fc407fe3d0
Your Name <bhabishamajhi1@gmail.com>
2026-04-12 19:22:01 +0545

    fix cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,14 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://online-bookstore-livid.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use("/api/books", require("./routes/bookRoutes"));
 app.use("/api/cart", require("./routes/cartRoutes"));
