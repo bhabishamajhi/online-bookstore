1bf7d3bcb5197d1d205a2f4cbf2bd36932b730c3
Your Name <bhabishamajhi1@gmail.com>
2026-04-15 21:33:30 +0545

    fix cart

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,18 @@ const Cart = require("./models/cartModel");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+
 app.use(express.json());
+
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://online-bookstore-549hg7w29-bhabishamajhis-projects.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 connectDB();
 
